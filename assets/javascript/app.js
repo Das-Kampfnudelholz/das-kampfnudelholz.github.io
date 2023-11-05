0b8d09d64e61ff5566ca1018aab6069d2f7ff048
Das-Kampfnudelholz <102672314+Das-Kampfnudelholz@users.noreply.github.com>
2023-11-05 10:37:30 +0100

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,13 @@
-// Made by Lummit - https://obnoxious.club/ | https://github.com/Lumm1t/ | Discord: Lummit#0201
-// Credits to expl0it, shellcode.team
-// GitHub: https://github.com/Lumm1t/obnoxious.club
-
 class _app {
   id = 0;
   videoElement = null;
   audioElement = null;
   musicVolume = 0.12;
   musicFadeIn = 4000;
-  skippedIntro = false;
+  skippedIntro = true;
   backgroundToggler = false;
   shouldIgnoreVideo = false;
-  effects = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble', 'jello'];
+  effects = ['C++', 'Py', 'C#', 'Node.JS', 'React', '', 'tada', 'wobble', 'jello'];
   brandDescription = ['11 years old kids team', 'pro roblox players', 'xane ddosers', 'trashtalkers', 'esoterik fanboys', 'hack vs hack team'];
 
   titleChanger = (text, delay) => {
